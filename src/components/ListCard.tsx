c359b9a1b106cf921f89a5fbd0b906e87b5eb96c
hrjaffery <hasnainrazajaffery13@gmail.com>
2023-05-01 14:07:37 +0500

    Make ListCard responsive

--- src/components/ListCard.tsx
+++ src/components/ListCard.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import {wp, hp} from '../utils';
 
 const ListCard = () => {
   return (
@@ -15,17 +16,18 @@ const ListCard = () => {
 
 const styles = StyleSheet.create({
   container: {
-    paddingLeft: 20,
-    paddingRight: 10,
-    backgroundColor: 'yellow',
-    margin: 10,
-    height: 270,
-    width: 140,
+    height: hp(35),
+    width: wp(35),
+    paddingLeft: wp(6),
+    paddingRight: wp(4),
+    marginTop: wp(4),
+    marginLeft: wp(4),
     borderRadius: 5,
+    backgroundColor: 'yellow',
   },
-  headerView: {marginTop: 30, justifyContent: 'center'},
+  headerView: {marginTop: hp(5), justifyContent: 'center'},
   line: {
-    width: 120,
+    width: wp(29),
     height: 2,
     backgroundColor: 'lightgray',
   },
