31162766dd00557ef74a3654b75a240e17a3745e
hrjaffery <hasnainrazajaffery13@gmail.com>
2023-05-01 13:05:23 +0500

    Refactor ListCard

--- src/components/ListCard.tsx
+++ src/components/ListCard.tsx
@@ -1,29 +1,34 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
 const ListCard = () => {
   return (
-    <View
-      style={{
-        paddingLeft: 20,
-        paddingRight: 10,
-        backgroundColor: 'yellow',
-        margin: 10,
-        height: 270,
-        width: 140,
-        borderRadius: 5,
-      }}>
-      <View style={{marginTop: 30, justifyContent: 'center'}}>
+    <View style={styles.container}>
+      <View style={styles.headerView}>
         <Text>Trip to Paris</Text>
       </View>
-      <View
-        style={{
-          width: 120,
-          height: 2,
-          backgroundColor: 'lightgray',
-        }}></View>
+      <View style={styles.line} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingLeft: 20,
+    paddingRight: 10,
+    backgroundColor: 'yellow',
+    margin: 10,
+    height: 270,
+    width: 140,
+    borderRadius: 5,
+  },
+  headerView: {marginTop: 30, justifyContent: 'center'},
+  line: {
+    width: 120,
+    height: 2,
+    backgroundColor: 'lightgray',
+  },
+});
 
 export default ListCard;
