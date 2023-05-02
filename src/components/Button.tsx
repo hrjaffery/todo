55eb261ce24d424a0e20646fab7a32bb3b32f9eb
hrjaffery <hasnainrazajaffery13@gmail.com>
2023-05-02 15:07:19 +0500

    Add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Button = () => {
+  return (
+    <View>
+      <Text>+</Text>
+      <Text>Add List</Text>
+    </View>
+  );
+};
+
+export default Button;
