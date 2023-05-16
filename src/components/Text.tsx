d64d0c5b046eac506fd539be9cb0635dd5a5f2e7
hrjaffery <hasnainrazajaffery13@gmail.com>
2023-05-16 13:21:50 +0500

    feature: add style and numberOfLines to the text

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -7,8 +7,12 @@ interface TextPropsTypes {
   numberOfLines?: number;
 }
 
-const CustomText = ({children, style, numberOfLines}: TextPropsTypes) => {
-  return <Text>{children}</Text>;
+const CustomText = ({children, style, numberOfLines = 0}: TextPropsTypes) => {
+  return (
+    <Text style={style} numberOfLines={numberOfLines}>
+      {children}
+    </Text>
+  );
 };
 
 export default CustomText;
